8a19c26e95ea74440626ddd2b058a944f47c9995
Mark Peters <mapeters101@earthlink.net>
2025-02-21 23:34:51 -0500

    Description for exercise05.

---
+++ exercise05.py
@@ -0,0 +1,26 @@
+"""
+Exercise 5
+You are given a list of tuples that each contain 4 values:
+
+(amount, currency, target_currency, exchange_rate)
+data = [
+    (100, 'USD', 'EUR', 0.83),
+    (100, 'USD', 'CAD', 1.27),
+    (100, 'CAD', 'EUR', 0.65)
+]
+Write code that converts the amount from its currency to its target_currency
+using the exchange_rate (which is the exchange rate for 1 currency in
+target_currency).
+
+Try to make your code as generic as possible (we'll see later how to use loops
+so we don't have to write three separate statements).
+
+In other words, you'll need three blocks of code here that are essentially
+almost identical.
+
+Use unpacking to assign the values in each tuple to variables.
+
+Your result for each row should print something like this out:
+
+100 USD = 83 EUR
+"""
