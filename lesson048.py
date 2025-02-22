0ea4dfd12d4abc91ebd0270cbde7526b208d483e
Mark Peters <mapeters101@earthlink.net>
2025-02-21 22:57:02 -0500

    Work on lesson048 from 3:00 to 5:00.

--- lesson048.py
+++ lesson048.py
@@ -8,3 +8,19 @@ print(t[len(t)-1])
 #t[2] = 40
 print('-'*80)
 
+t = ([1,2], [3,4])
+print(t)
+print(t[0], type(t[0]))
+#t[0] = 100
+l = t[0]
+print(l)
+l[0] = 100
+print(l)
+print(t)
+t[1][1] = 400
+print(t)
+
+
+
+
+
