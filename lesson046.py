52e65be63caa586686db99882f0d7d6e9c6a9a60
Mark Peters <mapeters101@earthlink.net>
2025-02-21 22:30:08 -0500

    Work on lesson046 to 2:25.

---
+++ lesson046.py
@@ -0,0 +1,17 @@
+l = [10, 20, 30, 40, 50]
+print(l)
+print(type(l))
+print(l[0])
+print(l[1])
+print(l[4])
+print(len(l))
+#print(l[5])
+print(l[-1])
+print(l[-2])
+print(l[-5])
+#print(l[-6])
+print('-'*80)
+
+
+
+
