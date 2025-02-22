3ed74bf4f8a775e3db2721f2d09a43217106ccc2
Mark Peters <mapeters101@earthlink.net>
2025-02-21 23:32:03 -0500

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,13 @@
+"""
+Exercise 2
+Concatenate the following tuples into a single one, but replacing the odd value
+s with zeros (0).
+
+t1 = 1, 2, 3, 4, 5, 6
+t2 = 7, 8, 9, 10
+t3 = 11, 12, 13, 14, 15, 16, 17
+You can assume that every tuple is a sequence of consecutive integers starting
+with an odd integer.
+
+Try to write your code to be as generic as possible.
+"""
