8b18e56b22b6c16d1e53cab8e1a4671aa60ff7f8
Mark Peters <mapeters101@earthlink.net>
2025-02-21 23:34:12 -0500

    Description for exercise04.

---
+++ exercise04.py
@@ -0,0 +1,16 @@
+"""
+Exercise 3
+Given the following matrix:
+
+m = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+Make this matrix into an identity matrix (setting the diagonal elements to 1).
+
+Your code should mutate m.
+
+Exercise 4
+Do the same problem as Exercise 3, but do not mutate m.
+"""
