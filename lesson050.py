f8e3f908edcab2383e5f65941c8434f507f3f83b
Mark Peters <mapeters101@earthlink.net>
2025-02-22 00:44:11 -0500

    Work on lesson050 from 2:40 to 4:15.

--- lesson050.py
+++ lesson050.py
@@ -3,5 +3,21 @@ b = "Python"
 a = "Python's best features:"
 #a = 'Python's best features:'
 a = 'Python\'s best features:'
+print('-'*80)
 
+
+s = 'Python rocks!'
+print(s[0])
+print(s[1])
+print(s[len(s) - 1])
+print(len(s))
+print(s[-1])
+print(s[-2])
+print(s[0])
+#s[0] = 'X'
+a = ''
+b = ""
+print(type(a),len(a))
+print(type(b),len(b))
+print('-'*80)
 
