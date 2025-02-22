98765597d0d62b019b04677344923373aed9b1da
Mark Peters <mapeters101@earthlink.net>
2025-02-21 22:36:52 -0500

    Complete work on lesson046 from 2:25.

--- lesson046.py
+++ lesson046.py
@@ -11,7 +11,25 @@ print(l[-2])
 print(l[-5])
 #print(l[-6])
 print('-'*80)
+print(l[len(l)-1])
+print(l[-1])
+print('-'*80)
 
+l = []
+print(l)
+print(type(l))
+print('-'*80)
 
+l = list()
+print(l)
+print(type(l))
+print('-'*80)
 
+l = [10, 20, 30, 40, 50]
+print(l[2])
+l[2] = 3.14
+print(l)
+l[-1] = 'Hello'
+print(l)
 
+# l[5] = 'Hello'
