6e2d1c7bb7c8c3c37667b7704c122f7195ef8234
Mark Peters <mapeters101@earthlink.net>
2025-02-21 23:31:07 -0500

    Description for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,11 @@
+"""
+Exercise 1
+Given the following string:
+
+s = 'FfEeDdCcBbAa'
+Create two new variables that contain just the lower and upper case letters of
+s respectively, in the correct alphabetical order, i.e:
+
+'ABCDEF'
+'abcdef'
+"""
