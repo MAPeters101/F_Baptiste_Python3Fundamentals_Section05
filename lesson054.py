4cf748e5847fe04d2c4ddb9c67e6c8ab4821a8e4
Mark Peters <mapeters101@earthlink.net>
2025-02-23 18:34:26 -0500

    Work on lesson054 to 3:00

---
+++ lesson054.py
@@ -0,0 +1,19 @@
+l = [10, 20, 3, 40, 50]
+print(l)
+l[2] = 30
+print(l)
+print()
+l = [1, 20, 30, 5, 6]
+print(l)
+print(l[1:3])
+l[1:3] = (2,3,4)
+print(l)
+print()
+print(l[1:3])
+l[1:3] = 'python'
+print(l)
+print("-"*80)
+
+
+
+
