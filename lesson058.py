9e2a8f373d82ea6dfd873466c9ba3a52bf1b25cb
Mark Peters <mapeters101@earthlink.net>
2025-03-03 22:51:26 -0500

    Work on lesson058 from 2:10 to 3:20.

--- lesson058.py
+++ lesson058.py
@@ -12,4 +12,23 @@ print()
 apr, apy = rate
 print(apr)
 print(apy)
+print('-'*80)
+
+
+a, b, c = 10, 3.14, 'abc'
+print(a)
+print(b)
+print(c)
+print()
+
+# a, b, c = 10, 20
+#a, b, c = 10, 20, 30, 40
+
+print()
+
+x, y, z = 'abc'
+print(x)
+print(y)
+print(z)
+print()
 print()
