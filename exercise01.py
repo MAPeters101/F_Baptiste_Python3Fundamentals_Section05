03b2a7028423e69d476fd8190e804755f7643306
Mark Peters <mapeters101@earthlink.net>
2025-03-04 18:54:01 -0500

    My first solution to exercise01.

--- exercise01.py
+++ exercise01.py
@@ -9,3 +9,21 @@ s respectively, in the correct alphabetical order, i.e:
 'ABCDEF'
 'abcdef'
 """
+
+s = 'FfEeDdCcBbAa'
+ll = []
+ul = []
+for char in s:
+    if char.isupper():
+        ul.append(char)
+    else:
+        ll.append(char)
+ll.sort()
+ul.sort()
+l = ''.join(ll)
+u = ''.join(ul)
+print(u)
+print(l)
+
+
+
