241b037cd252a53f503c0dcbf9ff0d520ae51e81
Mark Peters <mapeters101@earthlink.net>
2025-03-04 19:56:46 -0500

    First solution solution for exercise 4.

--- exercise04.py
+++ exercise04.py
@@ -14,3 +14,17 @@ Your code should mutate m.
 Exercise 4
 Do the same problem as Exercise 3, but do not mutate m.
 """
+import copy
+
+m = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+n = copy.deepcopy(m)
+for i in range(len(n[0])):
+    n[i][i] = 1
+print(m)
+print(n)
+
