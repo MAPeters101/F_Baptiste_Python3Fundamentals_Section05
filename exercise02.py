b7611004db7d21f0362048c738fbe8598c13aa60
Mark Peters <mapeters101@earthlink.net>
2025-03-04 19:40:21 -0500

    My first solution for exercise 2.

--- exercise02.py
+++ exercise02.py
@@ -11,3 +11,16 @@ with an odd integer.
 
 Try to write your code to be as generic as possible.
 """
+
+t1 = 1, 2, 3, 4, 5, 6
+t2 = 7, 8, 9, 10
+t3 = 11, 12, 13, 14, 15, 16, 17
+l = list(t1) +list(t2) + list(t3)
+
+for i in range(len(l)):
+    if l[i]%2 == 1:
+        l[i] = 0
+
+t = tuple(l)
+print(t)
+
