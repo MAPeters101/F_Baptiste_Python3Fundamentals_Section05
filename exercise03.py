e7f39eeb0654482b722cd8f6b48696a4bd148695
Mark Peters <mapeters101@earthlink.net>
2025-03-04 19:50:12 -0500

    First solution for exercise 3.

--- exercise03.py
+++ exercise03.py
@@ -11,3 +11,12 @@ Make this matrix into an identity matrix (setting the diagonal elements to 1).
 
 Your code should mutate m.
 """
+m = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+for i in range(len(m[0])):
+    m[i][i] = 1
+
+print(m)
